2b4dae920cb46ebd295739db6ac52af09f573fc4
nikhi <nikhi@Nikhil>
2024-07-20 01:15:27 +0530

    yy

--- GITMavenProject01/src/test/java/com/nt/test/AppTest.java
+++ GITMavenProject01/src/test/java/com/nt/test/AppTest.java
@@ -22,8 +22,8 @@ public class AppTest
 	   public void testWithNegatives()
 	   {
 		  Arithmetic ar=new Arithmetic();
-		  int expected=-40;
-	      int actual=ar.sum(-20,-20);
+		  int expected=-100;
+	      int actual=ar.sum(-50,-50);
 	      assertEquals(expected,actual);
 	   }
 	   
