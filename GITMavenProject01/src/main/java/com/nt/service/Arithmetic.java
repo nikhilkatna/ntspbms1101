14b75d851107405d7dceb6464c6f574ab7006400
nikhi <nikhi@Nikhil>
2024-07-19 22:51:24 +0530

    After Merging

--- GITMavenProject01/src/main/java/com/nt/service/Arithmetic.java
+++ GITMavenProject01/src/main/java/com/nt/service/Arithmetic.java
@@ -4,11 +4,8 @@ public class Arithmetic
 {
   public int sum(int x,int y)
   {
-<<<<<<< HEAD
 	  System.out.println("Arithmetic.sum().....Start");
-=======
 	  System.out.println("Arithmetic.sum()...Begin");
->>>>>>> branch 'master' of https://github.com/nikhilkatna/ntspbms1101.git
 	  return x+y;
   }
 }
