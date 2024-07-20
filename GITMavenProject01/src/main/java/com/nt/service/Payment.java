10adc8255218f830ac191de4c85a512d875e5cc4
nikhi <nikhi@Nikhil>
2024-07-21 00:20:45 +0530

    Dev-1 BharathPE

--- GITMavenProject01/src/main/java/com/nt/service/Payment.java
+++ GITMavenProject01/src/main/java/com/nt/service/Payment.java
@@ -12,8 +12,15 @@ public class Payment
 	   return "UPI Payment is done";
    }
    
+
    public String PayPallIntegration(String url)
    {
 	   return "PayPallIntegration is done";
+   }
+   
+   public String bharathPE()
+   {
+	   //Bharath pe Config's is going on
+	   return "BharathPE integration is done";
    }
 }
