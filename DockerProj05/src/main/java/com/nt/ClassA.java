c5f4ddaeb3e5130c24464aa950896bad185370a1
nikhi <nikhi@host.docker.internal>
2024-08-20 13:48:49 +0530

    initial commit

---
+++ DockerProj05/src/main/java/com/nt/ClassA.java
@@ -0,0 +1,10 @@
+package com.nt;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+ 
+		System.out.println("Welcome to Docker Maven Application...");
+	}
+
+}
--- DockerProj05/src/test/java/com/nt/DockerProj05ApplicationTests.java
+++ DockerProj05/src/test/java/com/nt/DockerProj05ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DockerProj02ApplicationTests {
+class DockerProj05ApplicationTests {
 
 	@Test
 	void contextLoads() {
